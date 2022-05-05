53ec9a4e32633a8af8918df01544ca6dd287e4f5
Kabelo Letlala <86678850+M3ntalist@users.noreply.github.com>
2022-05-05 15:01:08 +0200

    Kabelo Letlala's Profile
    Made some few changes, added CYF blocks(Social media links and Image carousel

--- script.js
+++ script.js
@@ -40,3 +40,34 @@ document.getElementById('button').addEventListener('click', (event) => {
   }
 
 });
+
+var images, numbers;
+
+
+images = ['https://saspecialist.southafrica.net/uploads/user_uploads/Mike.jpg', 'https://img4.travelagenciesfinder.com/522/383/399144435223836.jpg', 'https://image-prod.iol.co.za/resize/650x366/Michael-Letlala-Chief-Guide-and-Operations-Manager-Coffeebeans-Routes?source=https://xlibris.public.prod.oc.inl.infomaker.io:8443/opencontent/objects/6699856c-e307-5d22-ad86-b5f7f8891be8&operation=CROP&offset=0x46&resize=850x478', 'https://i.pinimg.com/736x/13/db/72/13db72aaf0527b793ad34393a50d6e1d.jpg'];
+numbers = [1, 2, 3, 4];
+let element_number = document.getElementById('number');
+element_number.innerText = numbers[0];
+let element_images = document.getElementById('images');
+element_images.setAttribute("src", images[0]);
+
+
+document.getElementById('next').addEventListener('click', (event) => {
+  numbers.push(numbers.shift());
+  images.push(images.shift());
+  let element_number2 = document.getElementById('number');
+  element_number2.innerText = numbers[0];
+  let element_images2 = document.getElementById('images');
+  element_images2.setAttribute("src", images[0]);
+
+});
+
+document.getElementById('previous').addEventListener('click', (event) => {
+  numbers.unshift(numbers.pop());
+  images.unshift(images.pop());
+  let element_images3 = document.getElementById('images');
+  element_images3.setAttribute("src", images[0]);
+  let element_number3 = document.getElementById('number');
+  element_number3.innerText = numbers[0];
+
+});
